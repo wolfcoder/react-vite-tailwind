2ce6d12512aee7eb0995b8cdcab311a7ad8e5147
Bams <bbg2021@gmail.com>
2023-05-20 22:44:32 +0800

    update description

--- src/components/TheHeader.jsx
+++
@@ -1,7 +0,0 @@
-export default function TheHeader() {
-    return (
-        <header style={{backgroundColor:"red"}}>
-            <h1 style={{color:"#FFF"}}>React App with Vite</h1>
-        </header>
-    )
-}
--- src/components/hero/TheHero01.jsx
+++ src/components/hero/TheHero01.jsx
@@ -3,10 +3,9 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Home', href: '#' },
+  { name: 'About', href: '#' },
+  { name: 'Company', href: '#' }
 ]
 
 export default function Hero01() {
@@ -113,7 +112,7 @@ export default function Hero01() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
+              Announcing our latest boiler plate.{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
@@ -122,18 +121,17 @@ export default function Hero01() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              React Tailwindcss Vite Starter Project
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+            This starter project provides a basic project structure with all the necessary dependencies and configuration files to get started with building a React application with Tailwind CSS and Vite.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
+              <a target="_blank" rel="noreferrer" 
+                href="https://github.com/wolfcoder/react-vite-tailwind"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Github Repo
               </a>
               <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
